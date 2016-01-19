96c47cd81c81dbc8f324a23742ea73c639987869
Xiang Wang <1846468966@qq.com>
2016-01-19 18:48:35 +0800

    ass字母转换

--- subtitles/ass_to_txt.py
+++ subtitles/ass_to_txt.py
@@ -2,20 +2,27 @@
 # -*- coding: utf-8 -*-
 # Xiang Wang @ 2016-01-17 22:07:20
 
-import ass
-import sys
+import ass, sys, re
 
 ass_file_path = sys.argv[1]
 txt_file_path = sys.argv[2]
+
+def assStypeGrep(text):
+    text = ''.join(re.split(r'{[\s\S]*?}',text))
+    text = text.replace('\\N','\n')
+    return text
 def main(doc,stylegrep=False):
     '''a function to change the ass doc object to text'''
     text = ''
     for i in doc.events:
-        text += i.text
-    return i
+        if stylegrep:
+            text += assStypeGrep(i.text) +'\n'
+        else:
+            text += i.text + '\n'
+    return text
 
 if __name__=='__main__':
     doc = ass.parse(open(ass_file_path))
-    text = main(doc, stylegrep=False)
+    text = main(doc, stylegrep=True)
     save_file_path = open(txt_file_path,'w')
     save_file_path.write(text)
