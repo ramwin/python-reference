639064c16489af4b1f3ebde1175421a52294d396
Xiang Wang <1846468966@qq.com>
2016-01-28 09:45:18 +0800

    nothing

--- subtitles/ass_to_txt.py
+++ subtitles/ass_to_txt.py
@@ -7,15 +7,23 @@ import sys
 
 ass_file_path = sys.argv[1]
 txt_file_path = sys.argv[2]
+def filter(text):
+    
+    
 def main(doc,stylegrep=False):
     '''a function to change the ass doc object to text'''
     text = ''
     for i in doc.events:
-        text += i.text
-    return i
+        if not stylegrep:
+            text = text +  i.text + '\n'
+        else:
+            text = text + filter(i.text) + '\n'
+    return text
 
 if __name__=='__main__':
     doc = ass.parse(open(ass_file_path))
     text = main(doc, stylegrep=False)
     save_file_path = open(txt_file_path,'w')
+    print(type(text))
     save_file_path.write(text)
+    save_file_path.close()
