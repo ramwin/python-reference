e86a5f6dd182600ed3145077623bcd166b9a48af
Xiang Wang <1846468966@qq.com>
2016-02-18 09:18:06 +0800

    nothing

--- 160202class调用自身方法.py
+++ 160202class调用自身方法.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 # Xiang Wang @ 2016-02-02 11:36:20
 
+''' 在class的内部，使用 self.<function>() 就可以调用本身'''
+
 class A():
     def __init__(self, value):
         self.value = value
@@ -16,3 +18,4 @@ a = A(1)
 a.add()
 print(a.get())
 print(a.add_get())
+print(a.get())
