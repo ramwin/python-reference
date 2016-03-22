d2fb9fef52b4559289587c171d605cd6c155ad0b
Xiang Wang <1846468966@qq.com>
2016-03-22 10:23:17 +0800

    python获取系统参数

--- 151208日志记录logging.py
+++ 151208日志记录logging.py
@@ -1,17 +1,14 @@
+#!/usr/bin/ent python3
 
 import logging
 
 logging.basicConfig(level=logging.DEBUG,
                 format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                 datefmt='%a, %d %b %Y %H:%M:%S',
-                filename='myapp.log',
+                filename='./log/test.log',
                 filemode='w')
     
 logging.debug('This is debug message')
 logging.info('This is info message')
 logging.warning('This is warning message')
  
-./myapp.log文件中内容为:
-Sun, 24 May 2009 21:48:54 demo2.py[line:11] DEBUG This is debug message
-Sun, 24 May 2009 21:48:54 demo2.py[line:12] INFO This is info message
-Sun, 24 May 2009 21:48:54 demo2.py[line:13] WARNING This is warning message
---
+++ 160320获取输入的参数.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Xiang Wang @ 2016-03-20 12:39:47
+
+import os
+
+def main():
+    print(1)
+
+if __name__ == '__main__':
+    main()
+    print(os.sys.argv[1])
