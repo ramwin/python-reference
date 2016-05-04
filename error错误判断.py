6870a9feea775b1c3b4a3b85ff8b8aabca61c59a
Xiang Wang <ramwin@qq.com>
2016-05-04 11:15:05 +0800

    nothing

---
+++ error错误判断.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Xiang Wang @ 2016-01-03 21:10:49
+
+def main():
+    '''错误判断的方法'''
+    try:
+        1/0
+    except ZeroDivisionError:
+        print('不能除以0')
+    except Exception as err:
+        print(err)
+# def main2():
+#     '''python2 的用法'''
+#     try:
+#         1/0
+#     except Exception,e:
+#         print e.message
+def main3():
+    '''python3 的用法, 最新版本的2也支持。'''
+    try:
+        1/0
+    except Exception as err:
+        print(err)
+
+# 自定义错误
+class TestError(Exception):
+    def __init__(self, name):
+        self.name = name
+        pass
+    def __str__(self):
+        return '自定义的错误 %s'%(self.name)
+raise TestError('error')
