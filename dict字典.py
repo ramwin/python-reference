dc05c725f019a68b828c893739001b51ca633c1b
Xiang Wang <ramwin@qq.com>
2016-05-04 11:15:05 +0800

    nothing

---
+++ dict字典.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Xiang Wang @ 2016-05-04 09:58:15
+
+dic = {'a':'b'}
+'a' in dic  # 判断有没有key
---
+++ string字符串.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Xiang Wang @ 2016-05-04 09:39:24
+
+'a,b,c'.split(',',1)    # 只拆分一次
