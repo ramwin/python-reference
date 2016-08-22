fc0a27c64d971c61a6c9810948df141e7cc961d9
Xiang Wang <ramwin@qq.com>
2016-08-22 09:48:33 +0800

    装饰器的使用

--- decorator装饰器.py
+++ decorator装饰器.py
@@ -15,14 +15,46 @@
 #     ''' main __docstring__ '''
 #     print(1)
 
+# 让装饰器带参数
+def deco(text):
+    def _deco(func):
+        def __deco(*args, **kwargs):
+            print('before myfunc() called.')
+            print(text)
+            func(*args, **kwargs)
+            print('after myfunc() called.')
+        return __deco
+    return _deco
 
-# main()
-# print(main.__doc__)
+@deco('text')
+def myfunc(text= 'no text' ):
+    print("myfunc() called")
+    print(text)
+
+class locker:
+    def __init__(self):
+        print("locker.__init__() should be not called.")
+
+    # @staticmethod
+    # def acquire():
+    #     print("locker.acquire() called.(静态方法)")
+
+    # @staticmethod
+    # def release():
+    #     print("locker.release() called.(不需要实例)")
+
+@deco(locker)
+def myfunc():
+    print('myfunc() called.')
+myfunc()
+# myfunc(text='text exists')
+# myfunc = deco(myfunc)
+# myfunc()
+
 
 from functools import update_wrapper
 ## 但是这样 docstring就变了
 def log(f):
-    print('log')
     def wrapper():
         print('wrap start')
         f()
@@ -34,5 +66,5 @@ def main():
     """ main docstring """
     print(1)
 
-main()
-print(main.__doc__)
+# main()
+# print(main.__doc__)
