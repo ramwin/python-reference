fe52d19ab352ad3cd8ce85fac6ad51d00f4b54ef
Xiang Wang <ubuntu@mac.cn>
2016-08-23 09:53:25 +0800

    nothing

--- requestsapi.py
+++ requestsapi.py
@@ -3,6 +3,7 @@
 # Xiang Wang @ 2016-08-17 13:32:31
 
 import requests
+import json
 headers = {
     'Authorization': 'Token 76ec5ef11050fefd51deb836b3c795b7ddca3bee',
     'Content-Type': 'Application/json',
@@ -13,12 +14,31 @@ params = {
 }
 
 data = {
+    'group'
 }
 
 def result(r):
     print('status_code: %d' % r.status_code)
     print('content: %s' % r.text)
 
+response = requests.post(
+    url='http://localhost:8000/api/v1/weeklypaper/1/todolist/1',
+)
+
+
+
+
+# # 获取 todo 列表
+# response = requests.get(
+#     url='http://localhost:8000/api/v1/todo/',
+#     headers=headers,)
+
+# 修改TODO
+# response = requests.patch(
+#     url='http://localhost:8000/api/v1/todo/2',
+#     headers=headers,
+#     data=json.dumps({'group':12}),
+#     )
 
 # # 获取周报
 # response = requests.get(
@@ -33,9 +53,6 @@ def result(r):
 #     headers=headers,
 # )
 # result(response)
-
-
-
 
 # # 上传group
 # url = 'http://localhost:8000/api/v1/todo/group'
@@ -58,3 +75,4 @@ def result(r):
 #     )
 # print(response.text)
 # 
+result(response)
