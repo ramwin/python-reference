6cad34ad3bac0894a8c7b2f0dde0613827e2237b
Xiang Wang <ubuntu@mac.cn>
2016-11-02 14:59:49 +0800

    Pillow画图

---
+++ testimport/circle循环import/A.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Xiang Wang @ 2016-10-28 13:13:52
+
+
+import B
+from B import D
+class C:
+    pass
---
+++ testimport/circle循环import/B.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Xiang Wang @ 2016-10-28 13:14:01
+
+class D:
+    pass
+
+from A import C
+
