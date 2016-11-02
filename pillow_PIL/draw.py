aab4a3fe131b0ed76f0c39e28deea1364d8e6229
Xiang Wang <ubuntu@mac.cn>
2016-11-02 17:37:47 +0800

    PIL画图

--- pillow_PIL/draw.py
+++ pillow_PIL/draw.py
@@ -7,7 +7,11 @@ from PIL import Image, ImageDraw
 import random
 
 
-def create_image(width=1000, height=1000, row=10, column=10, path="test.png", percent=0.4, background=0xffffff):
+def create_image(
+        width=1000, height=1000,
+        row=10, column=10,
+        path="test.png", percent=0.48,
+        background=0xffffff):
     """
     生成一张有各种小球图片
     """
@@ -24,7 +28,8 @@ def create_image(width=1000, height=1000, row=10, column=10, path="test.png", pe
     padding = 20  # 设置圆和边界之间的间隔
     for i in range(row):  # 第i行
         for j in range(column):  # 第j列
-            if random.randint(1, total - yellow_cnt - blue_cnt) > (yellow_total - yellow_cnt):
+            if random.randint(1, total - yellow_cnt - blue_cnt) > \
+               (yellow_total - yellow_cnt):
                 color = 'yellow'
             else:
                 color = 'blue'
