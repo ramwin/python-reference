6975c2b56b4b62125abf144ec91de97d3964f5dc
Xiang Wang <ramwin@qq.com>
2018-12-25 13:42:23 +0800

    fix bug

--- script/获取部门列表.py
+++ script/获取部门列表.py
@@ -3,6 +3,9 @@
 # Xiang Wang @ 2018-12-25 13:29:49
 
 
+import re
+
+
 class Dept(models.Model):
     title = models.CharField("部门名称", max_length=10)
     path = models.CharField("部门路径", max_length=100)
@@ -12,8 +15,17 @@ class DeptSerializer(serializers.ModelSerializer):
     children = serializers.SerializerMethodField()
 
     class Meta:
+        model = Dept
         fields = ["id", "title", "children"]
 
+    def get_children_qs(self, obj):
+        qs = Dept.objects.filter(path__startswith=obj.path)
+        results = []
+        for i in qs:
+            if re.match(r'{}\.\d+'.format(qs.path), i.path):
+                results.append(i)
+        return results
+
     def get_children(self, obj):
-        qs = Dept.objects.filter(path__endswith = ".{}".format(obj.id)).exclude(id=obj.id)
+        qs = self.get_children_qs(obj)
         return DeptSerializer(qs, many=True).data
