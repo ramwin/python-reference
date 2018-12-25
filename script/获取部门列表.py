5f7ee086b82534ac19467173fa062988cd17dc9c
Xiang Wang <ramwin@qq.com>
2018-12-25 13:37:39 +0800

    获取部门列表

---
+++ script/获取部门列表.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Xiang Wang @ 2018-12-25 13:29:49
+
+
+class Dept(models.Model):
+    title = models.CharField("部门名称", max_length=10)
+    path = models.CharField("部门路径", max_length=100)
+
+
+class DeptSerializer(serializers.ModelSerializer):
+    children = serializers.SerializerMethodField()
+
+    class Meta:
+        fields = ["id", "title", "children"]
+
+    def get_children(self, obj):
+        qs = Dept.objects.filter(path__endswith) = ".{}".format(obj.id)
+        return DeptSerializer(qs, many=True).data
