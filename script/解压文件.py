e9396c7ffa54d4413a49afb3c8603f667ef2192e
Xiang Wang <ramwin@qq.com>
2023-10-19 22:04:00 +0800

    feat: 移动文件名并修改标题

--- script/解压文件.py
+++ script/解压文件.py
@@ -9,6 +9,8 @@
 
 
 import logging
+import re
+
 from pathlib import Path
 
 from zipfile import ZipFile
@@ -16,6 +18,37 @@ from zipfile import ZipFile
 
 logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
+
+
+def de_nest_directory(path: Path):
+    """
+    如果path只有一个文件夹，就把文件夹内的内容拿到外面来
+    """
+    while True:
+        files = list(path.iterdir())
+        if len(files) == 1 and files[0].is_dir():
+            for content in files[0].iterdir():
+                content.rename(
+                        path.joinpath(content.name)
+                )
+            files[0].rmdir()
+        else:
+            break
+
+
+def change_name(path):
+    """
+    把path里面的所有内容，改成纯数字
+    第%d页.png => 0x.png
+    """
+    pattern = re.compile(r".*第(\d+)页.png")
+    for file in list(path.iterdir()):
+        match = pattern.match(file.name)
+        assert match, file
+        page = int(match.groups()[0])
+        file.rename(file.parent.joinpath(
+            f"{page:02d}.png"
+        ))
 
 
 def main():
@@ -30,9 +63,10 @@ def main():
             continue
         LOGGER.info("解压: %s", zip_file)
         if zip_file.suffix == ".zip":
-            with ZipFile(zip_file, "r") as myzip:
+            with ZipFile(zip_file, "r", metadata_encoding="gbk") as myzip:
                 myzip.extractall(target_dir)
-        break
+        de_nest_directory(target_dir)
+        change_name(target_dir)
 
 
 if __name__ == "__main__":
