fd9df83196cf1e60e651850dac32ea61b88e9ca2
Xiang Wang <ramwin@qq.com>
2023-11-17 23:49:00 +0800

    feat: 添加pytest

--- 快速安装通用包.sh
+++ 快速安装通用包.sh
@@ -4,5 +4,5 @@
 
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple 
 pip install -U pip
-pip install ipdb requests tldr redis click flake8 beautifulsoup4 pylint
+pip install ipdb requests tldr redis click flake8 beautifulsoup4 pylint pytest
 pip install django django-redis djangorestframework pylint-django
