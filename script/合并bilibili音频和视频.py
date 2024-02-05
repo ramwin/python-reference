3077011e2c0798e58d0bf94242211a19508d6165
Xiang Wang <ramwin@qq.com>
2024-02-05 23:05:05 +0800

    feat: 根据视频清晰度来选择文件

--- script/合并bilibili音频和视频.py
+++ script/合并bilibili音频和视频.py
@@ -3,22 +3,34 @@
 # Xiang Wang <ramwin@qq.com>
 
 
+"""转化缓存的bilibili视频"""
+
+
 import json
 from pathlib import Path
+import shutil
 import subprocess
 
+import click
 
-def main():
-    for directory in Path("17649289").iterdir():
+
+@click.command()
+@click.option("--source", prompt="输入需要转化的文件夹")
+def main(source):
+    """转化缓存的bilibili视频"""
+    for directory in Path(source).iterdir():
         with open(directory / "entry.json", encoding="utf8") as f:
             info = json.load(f)
         page = int(info["page_data"]["page"])
-        filename = f"转化/{page:02d}-" + \
-            info["page_data"]["part"] + ".mp4"
-        audio = directory / "64/audio.m4s"
-        video = directory / "64/video.m4s"
-        mp3 = audio.rename(directory / "64/audio.mp3")
-        mp4 = video.rename(directory / "64/video.mp4")
+        filename = Path(
+                "转化",
+                f'{page:02d}-{info["page_data"]["part"]}',
+        ).with_suffix('.mp4')
+        filename.parent.mkdir(exist_ok=True, parents=True)
+        audio = directory / str(info["video_quality"]) / "audio.m4s"
+        video = directory / str(info["video_quality"]) / "video.m4s"
+        mp3 = audio.rename(directory / str(info["video_quality"]) / "audio.mp3")
+        mp4 = video.rename(directory / str(info["video_quality"]) / "video.mp4")
         subprocess.run([
             "ffmpeg",
             "-i", mp4,
@@ -27,7 +39,8 @@ def main():
             "-c:a", "aac",
             filename,
         ], check=True)
+        shutil.rmtree(directory)
 
 
 if __name__ == "__main__":
-    main()
+    main()  # pylint: disable=no-value-for-parameter
