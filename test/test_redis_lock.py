ea33d1c127776d7df48279578a17380f93ac3c72
Xiang Wang <ramwin@qq.com>
2024-03-24 21:47:02 +0800

    feat: test redis lock

---
+++ test/test_redis_lock.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Xiang Wang <ramwin@qq.com>
+
+
+"""
+测试lock的时候，其他进程lock了怎么办
+"""
+
+
+import time
+from multiprocessing import Process
+
+from redis import Redis
+from redis.exceptions import LockError
+
+
+REDIS = Redis("localhost")
+
+
+def process1():
+    print("进程1开始")
+    with REDIS.lock("lock") as lock:
+        print("进程1拿到了锁")
+        time.sleep(5)
+        print("进程1结束")
+
+
+def process2():
+    print("进程2开始")
+    try:
+        with REDIS.lock("lock", blocking_timeout=0.1) as lock:
+            print("进程2拿到了锁")
+            time.sleep(5)
+            print("进程2结束")
+    except LockError as error:
+        print("进程2拿不到锁")
+
+
+if __name__ == "__main__":
+    p1 = Process(target=process1)
+    p2 = Process(target=process2)
+    p1.start()
+    p2.start()
