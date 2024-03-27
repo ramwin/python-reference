884fd50fe01f60d4676e6daefd5e51875ffc1356
Xiang Wang <ramwin@qq.com>
2024-03-27 22:39:40 +0800

    speed

--- test/test_time_speed.py
+++ test/test_time_speed.py
@@ -10,36 +10,51 @@ redis.get: %f 20952.91381253447
 """
 
 
-import redis
 import time
+import uuid
+
+import redis
 
 
 def test_add():
+    # 三千万 QPS
     start = time.time()
     size = 1_000_000
     a = 0
-    for i in range(size):
+    for _ in range(size):
         a += 1
     print("a+=1: ops: %f", size / (time.time() - start))
 
 
 def test_time():
+    # 一千万 QPS
     size = 1_000_000
     start = time.time()
-    for i in range(size):
+    for _ in range(size):
         a = time.time()
     print("time.time: ops: %f", size / (time.time() - start))
 
 
 def test_redis():
+    # 18K QPS
     c = redis.Redis()
     size = 10_000
     start = time.time()
-    for i in range(size):
+    for _ in range(size):
         c.get("foo")
     print("redis.get: %f", size / (time.time() - start))
+
+
+def test_uuid():
+    # 30万 QPS
+    size = 10_000
+    start = time.time()
+    for _ in range(size):
+        a = uuid.uuid4()
+    print("uuid: %f", size / (time.time() - start))
 
 
 test_add()
 test_time()
 test_redis()
+test_uuid()
