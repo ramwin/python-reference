5752c3d320dc78f758fac6c28362f11fb646789b
Xiang Wang <ramwin@qq.com>
2024-03-29 22:05:30 +0800

    feat: readthedocs

---
+++ run_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Xiang Wang(ramwin@qq.com)
+
+sphinx-autobuild -j auto --port 18000 . _build/html/
--- 启动文档服务.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Xiang Wang(ramwin@qq.com)
-
-sphinx-autobuild --port 18000 . build/html/
