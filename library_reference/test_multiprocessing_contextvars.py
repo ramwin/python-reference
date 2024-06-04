011e05b7ff3fc6ceec9155314f65c641c8898744
Xiang Wang <ramwin@qq.com>
2024-06-04 20:11:50 +0800

    单进程初始化

---
+++ library_reference/test_multiprocessing_contextvars.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Xiang Wang <ramwin@qq.com>
+
+
+import multiprocessing
+import os
+import time
+
+from contextvars import ContextVar
+
+
+var = ContextVar("var", default=False)
+
+
+def f(x):
+    time.sleep(x * 0.01)
+    if var.get() is False:
+        var.set(True)
+        print("第一次遇到进程", os.getpid())
+    print("处理", x)
+
+
+def main():
+    with multiprocessing.Pool(4) as p:
+        p.map(f, range(10))
+
+
+if __name__ == "__main__":
+    main()
