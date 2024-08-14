3bee0fcbb2c5c04f32593eac497b3f331247a611
Xiang Wang <ramwin@qq.com>
2024-08-14 20:33:21 +0800

    feat: filehandler

--- library_reference/logging_config.py
+++ library_reference/logging_config.py
@@ -11,7 +11,10 @@ handler = colorlog.StreamHandler()
 handler.setFormatter(colorlog.ColoredFormatter(
     "%(log_color)s%(levelname)s:%(name)s:%(message)s"
     ))
-file_handler = logging.FileHandler("info.log")
+file_handler = logging.FileHandler("info.log", mode="w")
+file_handler.setFormatter(logging.Formatter(
+     '%(asctime)s [%(module)s:%(lineno)d] %(levelname)s %(message)s'
+    ))
 logging.basicConfig(level=logging.INFO, handlers=[handler, file_handler])
 
 logger = logging.getLogger(__name__)
