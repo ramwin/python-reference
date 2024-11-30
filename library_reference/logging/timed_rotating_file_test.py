860dbfefcad2798b59eed1de0972ae1319f5006b
Xiang Wang <ramwin@qq.com>
2024-12-01 01:05:33 +0800

    feat: 设置interval

--- library_reference/logging/timed_rotating_file_test.py
+++ library_reference/logging/timed_rotating_file_test.py
@@ -3,6 +3,7 @@
 # Xiang Wang @ 2019-04-29 11:10:12
 
 import logging
+import datetime
 from logging.handlers import TimedRotatingFileHandler
 import time
 
@@ -13,8 +14,10 @@ logging.basicConfig(
 )
 file_handler1 = TimedRotatingFileHandler(
     filename="timelogger.log",
+    interval=1,
     backupCount=5,
     when="midnight",
+    atTime=datetime.time(0, 0, 0),
 )
 file_handler1.setFormatter(logging.Formatter(fmt="%(asctime)s %(message)s"))
 
