302165677af288e9bf39a455ee42dfc825d612d6
Xiang Wang <ramwin@qq.com>
2025-03-02 09:33:00 +0800

    typing alias

--- library_reference/test_typing_alias.py
+++ library_reference/test_typing_alias.py
@@ -8,8 +8,22 @@ from typing import NewType
 
 Age = NewType("Age", int)
 
+type MY_INT = int  # 3.10里面不加type
+
 
 def grow(age: Age) -> Age:
     # return age + 1  # 这样是报错的
     # return age + Age(1)  # 这样也不行
     return Age(age + 1)  # 这样才可以
+
+
+def test_alias(age: MY_INT) -> MY_INT:
+    return age + 3
+
+
+def main():
+    print(test_alias(3))
+
+
+if __name__ == "__main__":
+    main()
