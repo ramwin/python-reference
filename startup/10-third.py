7ad5d0a8b14a5ba09a64503b695e62f2fe20e18c
Xiang Wang <ramwin@qq.com>
2025-05-04 23:24:16 +0800

    ipython: cluster

--- startup/10-third.py
+++ startup/10-third.py
@@ -5,7 +5,10 @@
 
 import redis
 from redis import Redis
+from redis.cluster import RedisCluster
 
 from eth_utils.address import to_checksum_address
 
 REDIS = Redis(decode_responses=True)
+CLUSTER = RedisCluster.from_url('redis://localhost:7000', decode_responses=True)
+
