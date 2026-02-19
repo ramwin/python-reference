9943dfb25f4077eb7b1a14de27f7e16c7f9b011c
Xiang Wang <ramwin@qq.com>
2026-02-19 17:10:25 +0800

    redis cluster

--- startup/10-third.py
+++ startup/10-third.py
@@ -9,4 +9,4 @@ from redis import Redis, RedisCluster
 from eth_utils.address import to_checksum_address
 
 REDIS = Redis(decode_responses=True)
-REDIS_CLUSTER = Redis(decode_responses=True, host="localhost", port=7000)
+REDIS_CLUSTER = RedisCluster(decode_responses=True, host="localhost", port=7000)
